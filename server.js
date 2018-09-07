502d6f8832de75b19870d2b9819ba54d41f72e5c
Diana Sormani <diana.sorman+git@gmail.com>
2018-09-07 10:42:37 +0200

    small changes

--- server.js
+++ server.js
@@ -3,18 +3,17 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 const port = process.env.PORT || 3000;
-var app = express();
+let app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 // Registering middleware
-app.use(express.static(__dirname + '/public'));
-app.use( (req, res, next)=> {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
+app.use( (req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
   console.log(log);
-  fs.appendFile('server.log', log + '\n', (err)=>{
-    if (err){
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
       console.log('Unable to append to server.log file.');
     }
   });
@@ -26,16 +25,17 @@ app.use( (req, res, next)=> {
 //   })
 // });
 
+app.use(express.static(__dirname + '/public'));
 // Registering helpers
-hbs.registerHelper('getCurrentYear', ()=> {
+hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 });
 
-hbs.registerHelper('screamIt', (text)=> {
+hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
 
-// Managing the new pages
+// Managing Handlers: the new pages
 app.get('/', (req, res) => {
     res.render('home.hbs', {
       pageTitle: 'Home page',
